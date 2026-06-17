01a0d69fc513630b3f2fb15b59460bd1b9b420ca
Alvaro <lopezalvaro456123@gmail.com>
2026-06-17 13:39:49 -0300

    ft: test de reagendar. Camino feliz y error

--- src/domain/entities/evento.ts
+++ src/domain/entities/evento.ts
@@ -1,10 +1,12 @@
 export class Evento {
 	private readonly id: string;
 	private estado: string;
+	private fechaHora: Date;
 
-	constructor(id = '', estado = 'pendiente') {
+	constructor(id = '', estado = 'pendiente', fechaHora: Date) {
 		this.id = id;
 		this.estado = estado;
+		this.fechaHora = fechaHora;
 	}
 
 	obtenerId(): string {
@@ -14,8 +16,17 @@ export class Evento {
 	obtenerEstado(): string {
 		return this.estado;
 	}
+	obtenerFechaHora(): Date {
+		return this.fechaHora;
+	}
 
 	marcarComoCompletado(): void {
 		this.estado = 'completado';
+	}
+	cambiarFecha(fechaHora: Date): void{
+		if(this.estado === "completado"){
+			throw new Error("El evento ya esta completado");
+		}
+		this.fechaHora = fechaHora;
 	}
 }
